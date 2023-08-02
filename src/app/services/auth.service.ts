571e8484325942847080a01cf7a49b7fa6567dea
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-08-02 01:04:05 +0000

    Criação dos componentes e implementação header

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { PedagogoService } from './pedagogo.service';
+import { IPedagogo } from '../interfaces/IPedagogo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private pedagogoLogado: IPedagogo | undefined;
 
   constructor(private pedagogoService: PedagogoService) { }
 
@@ -13,10 +16,16 @@ export class AuthService {
     for (const pedagogo of pedagogos) {
       const emailCorreto = email === pedagogo.email;
       const senhaCorreta = senha === pedagogo.senha;
-      if (emailCorreto && senhaCorreta)
+      if (emailCorreto && senhaCorreta) {
+        this.pedagogoLogado = pedagogo;
         return;
+      }
     }
 
     throw new Error("Credenciais inválidas!");
+  }
+
+  obterNomePedagogoLogado() {
+    return this.pedagogoLogado?.nome;
   }
 }
